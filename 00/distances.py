698fc39b5b9fb9d90ef865528f8c338ba0677c68
Peter Dobosi <dobosipeter96@gmail.com>
2024-12-27 23:17:22 +0100

    Second challenge of first day wip

--- 00/distances.py
+++ 00/distances.py
@@ -32,7 +32,7 @@ def sort_input(input_data: pd.DataFrame) -> pd.DataFrame:
 
 def calculate_deltas(input_data: pd.DataFrame) -> pd.DataFrame:
     """
-    Calculate the absolute distances between tge two columns.
+    Calculate the absolute distance between the two columns.
 
     :param input_data: The input data to use.
 
@@ -41,10 +41,21 @@ def calculate_deltas(input_data: pd.DataFrame) -> pd.DataFrame:
     input_data['delta'] = (input_data['left'] - input_data['right']).abs()
     return input_data
 
-# Sum the deltas
+def calculate_occurences(input_data: pd.DataFrame) -> dict:
+    """
+    Calcualte the amount of times each item in the left column appears in the right column.
+
+    :param input_data: The input data to use.
+
+    :return: A dictionary keyed by the items in the left column and their value counts in the right column as values.
+    """
+    value_counts = input_data.right.value_counts()
+    print(value_counts)
+
 
 if __name__ == '__main__':
     parsed_input = parse_input(Path('input.txt'))
     sorted_input = sort_input(parsed_input)
     calculated_deltas = calculate_deltas(sorted_input)
     print(f'Sum of deltas: {calculated_deltas.delta.sum()}')
+    calculate_occurences(sorted_input)
