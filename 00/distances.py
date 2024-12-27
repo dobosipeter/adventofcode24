aba261f22b8e5eb51d891ea4ee52557b5f0f5792
peter <dobosipeter@edu.bme.hu>
2024-12-28 00:31:44 +0100

    Second challenge of the first day completed.

--- 00/distances.py
+++ 00/distances.py
@@ -10,7 +10,7 @@ def parse_input(input_path: Path) -> pd.DataFrame:
 
     :return: The parsed input.
     """
-    parsed_data = pd.read_csv(input_path, sep='   ', header=None)
+    parsed_data = pd.read_csv(input_path, sep='   ', header=None, engine='python')
     parsed_data.columns = ['left', 'right']
 
     return parsed_data
@@ -49,8 +49,23 @@ def calculate_occurences(input_data: pd.DataFrame) -> dict:
 
     :return: A dictionary keyed by the items in the left column and their value counts in the right column as values.
     """
-    value_counts = input_data.right.value_counts()
-    print(value_counts)
+    value_counts = input_data.right.value_counts().to_dict()
+    for _, value in input_data.left.items():
+        if value not in value_counts:
+            value_counts[value] = 0
+    return value_counts
+
+def calculate_similarity_score(input_data: pd.DataFrame, occurence_counts: dict) -> pd.DataFrame:
+    """
+    Calculate the similarity score of the items in the left column, weighted by their occurence counts.
+
+    :param input_data: The input data to use.
+    :param occurence_counts: The dictionary of occurence counts.
+
+    :return: The DataFrame augumented with the results.
+    """
+    input_data['similarity'] = input_data['left'] * input_data.left.map(occurence_counts)
+    return input_data
 
 
 if __name__ == '__main__':
@@ -58,4 +73,6 @@ if __name__ == '__main__':
     sorted_input = sort_input(parsed_input)
     calculated_deltas = calculate_deltas(sorted_input)
     print(f'Sum of deltas: {calculated_deltas.delta.sum()}')
-    calculate_occurences(sorted_input)
+    occurences = calculate_occurences(sorted_input)
+    similarity_scores = calculate_similarity_score(sorted_input, occurences)
+    print(f'Sum of similarity scores: {similarity_scores.similarity.sum()}')
